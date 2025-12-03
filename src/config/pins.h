91a5c0b5ef8777b6cf801af05f83ebaa2c800b78
allkinn <ansarmubaroq@gmail.com>
2025-12-03 09:36:45 +0800

    Change keypad pin definitions to integers
    Updated keypad pin definitions to use integer values instead of pin names.

--- src/config/pins.h
+++ src/config/pins.h
@@ -5,11 +5,11 @@
 #define KEYPAD_ROW1 A1
 #define KEYPAD_ROW2 A2  
 #define KEYPAD_ROW3 A3
-#define KEYPAD_ROW4 D6
-#define KEYPAD_COL1 D7
-#define KEYPAD_COL2 D8
-#define KEYPAD_COL3 D12
-#define KEYPAD_COL4 D13
+#define KEYPAD_ROW4 6
+#define KEYPAD_COL1 7
+#define KEYPAD_COL2 8
+#define KEYPAD_COL3 12
+#define KEYPAD_COL4 13
 
 // LCD I2C (A4=SDA, A5=SCL automatic)
 #define LCD_ADDRESS 0x27
@@ -29,4 +29,5 @@
 #define HALL_PIN A0
 
 // Output
+#define LED_GREEN 0  // D0, sacrifice Serial RX
 #define BUZZER_PIN 1  // D1, sacrifice Serial TX
